c3888705e2ab0d8c917a78ce954e4c8ebe8fea46
David Dinh <dmdinh22@gmail.com>
2019-08-24 21:06:42 -0700

    channel buffering notes

--- channel-buffering.go
+++ channel-buffering.go
@@ -11,3 +11,8 @@ func main() {
 	fmt.Println(<-messages)
 	fmt.Println(<-messages)
 }
+
+// default channels are unbuffered - only accept sends (chan <-)
+// if there is a corresponding receive (<- chan)
+//! buffered channels accept a limited number of vals
+//! w/o a corresponding receiver for those values
